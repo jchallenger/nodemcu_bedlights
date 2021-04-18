da6fab196c537b359fd99e71d7c4c4c4683fe544
Justin Challenger <challenger.justin@gmail.com>
2021-04-17 20:02:47 -0400

    trim main + wifi events

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
 #include <Arduino.h>
+#include <AsyncElegantOTA.h>
 #include <ESP8266WiFi.h>
 #include <ESPAsyncTCP.h>
 #include <ESPAsyncWebServer.h>
-#include <AsyncElegantOTA.h>
 #include <FastLED.h>
-#include <vector>
 #include <string>
+#include <vector>
 
 #include <LEDStrip.h>
 
@@ -19,92 +19,64 @@ unsigned long prevTime = millis();
 unsigned long ticks = 0;
 
 CRGB leds[NUM_LEDS];
-LEDStrip * myLEDs;
+LEDStrip *myLEDs;
 
-const char* ssid = "HomeNet-24";
-const char* password = "fourwordsalluppercase";
+WiFiEventHandler mConnectHandler, mDisConnectHandler, mGotIpHandler;
+const char *ssid = "HomeNet-24";
+const char *password = "fourwordsalluppercase";
+int reqConnect = 0;
+int isConnected = 0;
+const long reqConnectNum = 15; // number of intervals to wait for connection
 
 AsyncWebServer server(80);
 
+void onConnected(const WiFiEventStationModeConnected &event) {
+  Serial.println("Connected to AP.");
+  isConnected = 1;
+}
+
+void onDisconnect(const WiFiEventStationModeDisconnected &event) {
+  Serial.println("WiFi On Disconnect.");
+  isConnected = 0;
+}
+
+void onGotIP(const WiFiEventStationModeGotIP &event) {
+  Serial.print("Got Ip: ");
+  Serial.println(WiFi.localIP());
+  isConnected = 2;
+}
+
 void setup(void) {
   Serial.begin(9600);
-  
+
   // Start LED
-  FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, NUM_LEDS);  // GRB ordering is typical
+  FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds,
+                                          NUM_LEDS); // GRB ordering is typical
   myLEDs = new LEDStrip();
 
   // Wifi indicators
-  for(int ii=0; ii<10; ii++){
-    leds[ii] = CRGB(100,0,0);
+  for (int ii = 0; ii < 20; ii++) {
+    leds[ii] = CRGB(0, 200, 50);
     myLEDs->addLight(new led_animation(leds[ii]));
   }
-
-  for(int ii=10; ii<20; ii++){
-    leds[ii] = CRGB(0,0,250);
-    myLEDs->addLight(new led_animation(leds[ii]));
-  }
-  for(int ii=20; ii<30; ii++){
-    leds[ii] = CRGB(100,0,100);
-    myLEDs->addLight(new led_animation(leds[ii], PLAYMODE::PING));
-  }
-  for(int ii=30; ii<40; ii++){
-    leds[ii] = CRGB(0,100,100);
-    led_animation * li = new led_animation(leds[ii], PLAYMODE::PLAY);
-    li->setDuration(15*1000);
+  for (int ii = 20; ii < NUM_LEDS; ii++) {
+    leds[ii] = CRGB(100, 0, 100);
+    led_animation_blink *li = new led_animation_blink(leds[ii], PLAYMODE::PING);
+    li->setColorRange(CRGB(200, 0, 200), CRGB(0, 50, 0));
+    li->setDuration(5 * 1000);
     myLEDs->addLight(li);
   }
-
-  // animations..
-  for(int ii=40; ii<NUM_LEDS; ii++){
-    leds[ii] = CRGB(10,100,200);
-    myLEDs->addLight(new led_animation_blink(leds[ii]));
-  }
-
   FastLED.show();
   delay(500);
-  for(int ii=10; ii<NUM_LEDS; ii++){
-    leds[ii] = CRGB(0,50,50);
-  }
-  FastLED.show();
-  delay(500);
-  for(int ii=10; ii<NUM_LEDS; ii++){
-    leds[ii] = CRGB(0,100,100);
-  }
-  FastLED.show();
-  delay(500);
-  for(int ii=10; ii<NUM_LEDS; ii++){
-    leds[ii] = CRGB(10,10,10);
-  }
-  FastLED.show();
-  delay(1000);
-  
 
+  // Wifi init
+  WiFi.disconnect();
+  WiFi.persistent(false);
+  mConnectHandler = WiFi.onStationModeConnected(onConnected);
+  mDisConnectHandler = WiFi.onStationModeDisconnected(onDisconnect);
+  mGotIpHandler = WiFi.onStationModeGotIP(onGotIP);
 
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  Serial.println("");
-
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    // Wifi indicators
-    for(int ii=0; ii<10; ii++){
-      leds[ii] = CRGB(100,0,0);
-    }
-    FastLED.show();
-    delay(100);
-    for(int ii=0; ii<10; ii++){
-      leds[ii] = CRGB(10,0,0);
-    }
-    FastLED.show();
-    delay(100);
-  }
-
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-
+  // HTTP Server
   server.on("/r", HTTP_GET, [](AsyncWebServerRequest *request) {
     request->send(200, "text/plain", "Switching to Red");
   });
@@ -119,46 +91,47 @@ void setup(void) {
   });
 
   server.on("/update", HTTP_GET, [](AsyncWebServerRequest *request) {
-    //Check if GET parameter exists
-    if(request->hasParam("brightness")){
-      AsyncWebParameter* p = request->getParam("brightness");
+    // Check if GET parameter exists
+    if (request->hasParam("brightness")) {
+      AsyncWebParameter *p = request->getParam("brightness");
     }
     request->send(200, "text/plain", "Updates Complete!");
   });
 
-  
   // Server with different default file
   server.serveStatic("/", SPIFFS, "/www/").setDefaultFile("index.html");
 
-  AsyncElegantOTA.begin(&server);    // Start ElegantOTA
+  AsyncElegantOTA.begin(&server); // Start ElegantOTA
   server.begin();
   Serial.println("HTTP server started");
-
-
-}
-
-
-float lerp(float a, float b, float f) 
-{
-    return (a * (1.0f - f)) + (b * f);
 }
 
 void loop(void) {
   ticks++;
   AsyncElegantOTA.loop();
   unsigned long now = millis();
-  myLEDs->update((now - prevTime));
+  myLEDs->update(now - prevTime);
   prevTime = now;
 
-  if((now - lTime) >= FPS_RATE ){
+  if ((now - lTime) >= FPS_RATE) {
+    reqConnect++;
+
+    if (WiFi.status() != WL_CONNECTED && reqConnect > reqConnectNum &&
+        isConnected < 2) {
+      WiFi.begin(ssid, password);
+      Serial.print(".");
+    }
+
+    if (isConnected < 2) {
+    }
+
     // Analog IN
     int sensorValue = analogRead(A0);
-    Serial.print("now=");
-    Serial.print(now);
-    Serial.print("  sincelast= ");
-    Serial.print(now - lTime);
-    Serial.print("  upf= ");
-    Serial.print(ticks);
+    Serial.print((String) "now=" + now + "  sincelast= " + (now - lTime) +
+                 "  upf= " + ticks);
+    Serial.print((String) "  LED[50]=" + myLEDs->getLight(50)->getValue() +
+                 " - " + myLEDs->getLight(50)->getProgress());
+    /*
     Serial.print("  LED[1]=");
     Serial.print(myLEDs->getLight(1)->getProgress());
     Serial.print("  LED[10]=");
@@ -167,13 +140,14 @@ void loop(void) {
     Serial.print(myLEDs->getLight(100)->getValue());
     Serial.print("  LED[150]=");
     Serial.print(myLEDs->getLight(150)->getProgress());
+    */
+    Serial.println((String) "  sensor = " + sensorValue);
 
-    Serial.print("  sensor = ");
-    Serial.println(sensorValue);
-
+    // set Brightness
+    myLEDs->setBrightness(sensorValue / 1024.f);
     // apply LEDs
     myLEDs->display();
-    
+
     // Turn the LED on, then pause
     FastLED.show();
     ticks = 0;
